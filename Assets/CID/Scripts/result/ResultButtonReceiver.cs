836b107f332ad1aa3b27e5f5684b35e7c505fc77
huraicid <shu.tateuchi@gmail.com>
2024-01-24 22:18:10 +0900

    ファイル名、クラス名を修正

--- Assets/CID/Scripts/result/ResultButtonReceiver.cs
+++ Assets/CID/Scripts/result/ResultButtonReceiver.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class ResultButtonReceiverScript : MonoBehaviour
+public class ResultButtonReceiver : MonoBehaviour
 {
     void Update()
     {
