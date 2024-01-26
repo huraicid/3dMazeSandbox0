dcd07a08759c3e856e3e668bd86d1d5789ded9c2
huraicid <shu.tateuchi@gmail.com>
2024-01-26 23:54:31 +0900

    GameManager.csのリファクタリング

--- Assets/CID/Scripts/gameManager/gameManager.cs
+++ Assets/CID/Scripts/gameManager/gameManager.cs
@@ -1,22 +1,22 @@
-using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
+/// <summary>
+/// ダンジョン内のゲーム処理を管理するクラスです。
+/// </summary>
 public class GameManager : MonoBehaviour
 {
     // フロアをロードするときの処理
     void Start()
     {
+        // タイマーが起動されていなければ起動する
         if(!GameVariables.isTimerActive)
         {
             GameVariables.isTimerActive = true;
         }
 
-        // イベントにイベントハンドラーを追加
-        SceneManager.sceneLoaded += SceneLoaded;
-        Debug.Log("読み込みOK");
-
+        // フロア情報が初期化前なら初期化する
         if (GameVariables.floor < 1)
         {
             GameVariables.floor = 1;
@@ -33,16 +33,17 @@ public class GameManager : MonoBehaviour
         }
 
         // Floor情報のUIを更新
-        updateFloorUI();
+        UpdateFloorUI();
 
     }
 
     // マイフレーム実行される処理
     private void Update()
     {
-        // game abort
+        // Escキーが押されたときゲームを終了する
         if(Input.GetKeyDown(KeyCode.Escape))
         {
+            GameVariables.isTimerActive = false;
             SceneManager.LoadScene("cid_entryPoint");
         }
 
@@ -53,18 +54,19 @@ public class GameManager : MonoBehaviour
         }
     }
 
-    // 次のフロアに進むときの処理
+    /// <summary>
+    /// 次のフロアに進むときの処理です。
+    /// </summary>
+    /// <param name="nextFloorName">次のフロアの名前</param>
     public void GoToNextFloor(string nextFloorName)
     {
-        Debug.Log("isGoal:" + (GameVariables.maxFloor == GameVariables.floor));
+        // 最上階から次のフロアに進むときはクリア画面に遷移する
         if(GameVariables.maxFloor == GameVariables.floor)
         {
             GameVariables.isTimerActive = false;
             SceneManager.LoadScene("result");
             return;
         }
-
-        Debug.Log("GoToNextFloor()が呼び出されました。next: " + nextFloorName);
 
         // フェードアウトを開始する
         FadeOut();
@@ -77,13 +79,13 @@ public class GameManager : MonoBehaviour
         SceneManager.LoadScene(nextFloorName);
     }
 
-    // フェードアウトする
+    /// <summary>
+    /// フェードアウトします。
+    /// </summary>
     private void FadeOut()
     {
         Image fadeImage = null;      // フェード用のUIパネル（Image）
         float fadeDuration = 10.0f;    // フェードの完了にかかる時間
-
-        Debug.Log("FadeOut()が呼び出されました");
 
         GameObject foundObject = GameObject.Find("Image");
         // 取得したGameObjectがnullでないことを確認
@@ -92,7 +94,6 @@ public class GameManager : MonoBehaviour
             UnityEngine.Debug.LogWarning("指定された名前のGameObjectが見つかりませんでした。");
             return;
         }
-        Debug.Log("フェードアウト用のゲームオブジェクトOK");
 
         // Imageコンポーネントを取得
         Image imageComponent = foundObject.GetComponent<Image>();
@@ -101,7 +102,6 @@ public class GameManager : MonoBehaviour
             UnityEngine.Debug.LogWarning("GameObjectにImageコンポーネントがアタッチされていません。");
             return;
         }
-        Debug.Log("フェードアウト用のImageコンポーネントOK");
 
         // 取得したImageコンポーネントを使用する
         fadeImage = imageComponent;
@@ -116,32 +116,25 @@ public class GameManager : MonoBehaviour
         }
     }
 
-    // フロア情報を取得する
-    private static void updateFloorUI()
+    /// <summary>
+    /// フロア情報を取得します。
+    /// </summary>
+    private static void UpdateFloorUI()
     {
+        // Floor UIを取得してアタッチしたScriptを実行
         GameObject floorTextObject = GameObject.Find("FloorText");
         if (floorTextObject == null)
         {
             Debug.LogWarning("floorTextが取得できませんでした");
             return;
         }
-        Debug.Log("floor Text取得できました");
 
-        //
         FloorUIScript script = floorTextObject.GetComponent<FloorUIScript>();
         if (script == null)
         {
             Debug.LogWarning("floorUIScriptが取得できませんでした");
             return;
         }
-
         script.updateFloorText();
-    }
-
-    // イベントハンドラー（イベント発生時に動かしたい処理）
-    void SceneLoaded(Scene nextScene, LoadSceneMode mode)
-    {
-        Debug.Log(nextScene.name);
-        Debug.Log(mode);
     }
 }
